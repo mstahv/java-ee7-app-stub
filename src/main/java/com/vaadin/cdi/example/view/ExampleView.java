a067da7e3c0555cf7185751cf7a3bd409ed85bbd
Niki Hansche <mf-developing@hansche.de>
2014-10-27 16:06:30 +0100

    Remove boilerplate-headers

--- src/main/java/com/vaadin/cdi/example/view/ExampleView.java
+++ src/main/java/com/vaadin/cdi/example/view/ExampleView.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.vaadin.cdi.example.view;
 
 import com.vaadin.cdi.CDIView;
